d7290d047ff3537ba5c8c3512b9c4d2707f2ecf7
Nipun Kul <nipunkul11@gmail.com>
2025-03-08 11:01:26 +0530

    day3_micbtn/sound

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# playing assistant sound function
+def playassistantsound():
+    music_dir = "D:\\Jarvis\\www\\assests\\audio\\start_sound.mp3"
+    playsound(music_dir)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import eel
 
+from engine.features import*
+
 eel.init("www")
+
+playassistantsound()
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
