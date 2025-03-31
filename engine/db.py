e391d0ecc105bb703e93d9989c47f3738c0e6778
Nipun Kul <nipunkul11@gmail.com>
2025-03-31 23:40:03 +0530

    SQLite Database intergeration

--- engine/db.py
+++ engine/db.py
@@ -1,11 +1,25 @@
-import _sqlite3
+import sqlite3
 
-con = _sqlite3.connect("NOVA.db")
+con = sqlite3.connect("NOVA.db")
 cursor = con.cursor()
 
 query = "CREATE TABLE IF NOT EXISTS sys_command(id interger primary key, name VARCHAR(100),path VARCHAR(1000))"
 cursor.execute(query)
 
-query = "INSERT INTO sys_command (id, name, path) VALUES (null, 'android studio', 'C:\\Program Files\\Android\\Android Studio\\bin\\studio64.exe')"
+# query = "INSERT INTO sys_command VALUES (null, 'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.EXE')"
+# cursor.execute(query)
+# con.commit()
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id interger primary key, name VARCHAR(100), url VARCHAR(1000))"
+cursor.execute(query)
+
+    # to update query
+    # update_query = "UPDATE web_command SET name = 'canva' WHERE name = 'camva'"
+    # cursor.execute(update_query)
+
+    # # Commit the changes
+    # con.commit()
+
+query = "INSERT INTO web_command VALUES (null, 'youtube', 'https://www.youtube.com/')"
 cursor.execute(query)
 con.commit()
